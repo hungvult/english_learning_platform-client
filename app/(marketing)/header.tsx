3e1cdcf40fa7832637d4731f0f1c95d05c0e1d52
hungvult <tuanhungvu0912@gmail.com>
2026-04-16 22:08:04 +0700

    app/(marketing): add index page

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export const Header = () => {
+  return (
+    <header
+      className={cn(
+        "h-20 w-full border-b-2 border-slate-200 px-4",
+        "mt-0"
+      )}
+    >
+      <div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
+        <Link href="/" className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
+          <Image src="/mascot.svg" alt="Mascot" height={40} width={40} />
+
+          <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
+            Lingo
+          </h1>
+        </Link>
+
+        <div className="flex gap-x-3">
+          <Button size="lg" variant="ghost" asChild>
+            <Link href="/login">Login</Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
