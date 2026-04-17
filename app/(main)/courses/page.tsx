e1ee16ad17362f9c12d35e2cf14c0b9ace1ffdf9
hungvult <tuanhungvu0912@gmail.com>
2026-04-17 08:29:47 +0700

    feat(app/(main)/courses): add back button (nagigate to /learn page)

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,5 +1,8 @@
 import { api } from "@/lib/api";
 import { CoursesList } from "@/components/courses-list";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 type UserCoursesResponse = {
   activeCourseId?: string;
@@ -20,7 +23,15 @@ const CoursesPage = async () => {
   }
 
   return (
-    <div className="h-full">
+    <div className="h-full max-w-[912px] mx-auto pb-10">
+      <div className="pt-6 px-3">
+        <Button variant="ghost" asChild>
+          <Link href="/learn">
+            <ArrowLeft className="h-5 w-5 mr-2" />
+            Return to Learn
+          </Link>
+        </Button>
+      </div>
       <CoursesList
         courses={data.courses}
         activeCourseId={data.activeCourseId}
